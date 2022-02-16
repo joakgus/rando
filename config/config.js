46774856752fe49179992f1e607be1a1c5c19591
joakgus <joakgus@kth.se>
2022-02-16 21:27:52 +0100

    login with database done!!!

--- config/config.js
+++ config/config.js
@@ -1,23 +1,30 @@
 module.exports = {
-    secretKey: process.env.SECRET_KEY,
-    db: {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
+    development: {
+        secretKey: process.env.SECRET_KEY,
+        db: {
+            dialect: 'postgres',
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            },
+            pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000,
+                idle: 10000
             }
-        },
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
         }
+    },
+    production:{
+        use_env_variable: DATABASE_URL,
+        dialect: 'postgres',
+        operatorsAliases: false
     }
 }
--- model/index.js
+++ model/index.js
@@ -1,22 +1,28 @@
 const config = require("../config/config");
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-    config.db.database,
-    config.db.username,
-    config.db.password,
-    {
-        host: config.db.host,
-        dialect: config.db.dialect,
-        dialectOptions: config.db.dialectOptions,
-        operatorsAliases: false,
-        pool: {
-            max: config.db.pool.max,
-            min: config.db.pool.min,
-            acquire: config.db.pool.acquire,
-            idle: config.db.pool.idle
+var sequelize = "";
+if(process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL);
+}
+else{
+    sequelize = new Sequelize(
+        config.db.database,
+        config.db.username,
+        config.db.password,
+        {
+            host: config.db.host,
+            dialect: config.db.dialect,
+            dialectOptions: config.db.dialectOptions,
+            operatorsAliases: false,
+            pool: {
+                max: config.db.pool.max,
+                min: config.db.pool.min,
+                acquire: config.db.pool.acquire,
+                idle: config.db.pool.idle
+            }
         }
-    }
-);
+    );
+}
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
