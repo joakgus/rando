6b19b8f0e7039d5c6b299f8d4eadca20090f4f5d
joakgus <joakgus@kth.se>
2022-02-16 22:22:56 +0100

    login with database done!!!

--- client/src/model/UserGetter.js
+++ client/src/model/UserGetter.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth/";
+const API_URL = "http://localhost:" + process.env.PORT + "/api/auth/";
 
 class UserGetter {
     login(username, password) {
--- client/src/model/UserService.js
+++ client/src/model/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import authHeader from './authHeader';
-const API_URL = 'http://localhost:5000/api/test/';
+const API_URL = 'http://localhost:'+process.env.PORT+'/api/test/';
 class UserService {
     getPublicContent() {
         return axios.get(API_URL + 'all');
