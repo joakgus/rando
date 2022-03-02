fce00c4acd5d20d02ae2ad0991ce73de0b3b6584
joakgus <joakgus@kth.se>
2022-03-02 14:10:48 +0100

    Name changes on .env references

--- model/index.js
+++ model/index.js
@@ -8,11 +8,11 @@ if(process.env.DATABASE_URL) {
 }
 else{
     sequelize = new Sequelize(
-        process.env.DB_DATA,
-        process.env.DB_USER,
-        process.env.DB_PASS,
+        process.env.DB_NAME,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD,
         {
-            host: process.env.DB_NAME,
+            host: process.env.DB_HOST,
             dialect: 'postgres',
             dialectOptions: {
                 ssl: {
