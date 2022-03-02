c6ba4fb1adddb05289f9a36aae1b323253896e8e
joakgus <joakgus@kth.se>
2022-03-02 14:14:01 +0100

    sent local to production. Changed that now.

--- client/src/model/UserService.js
+++ client/src/model/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import authHeader from './authHeader';
-const API_URL = "http://localhost:5000/api/test/"//'https://protected-wildwood-15719.herokuapp.com/api/test/';
+const API_URL = /*"http://localhost:5000/api/test/"*/'https://protected-wildwood-15719.herokuapp.com/api/test/';
 class UserService {
     getPublicContent() {
         return axios.get(API_URL + 'all');
